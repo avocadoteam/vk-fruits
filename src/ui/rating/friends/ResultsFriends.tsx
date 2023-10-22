c0c114a2c9b827f9ce730a30285d875a8df6a5b9
Kon <whynunu@why.com>
2023-10-22 19:39:48 +0200

    changed rating layout

--- src/ui/rating/friends/ResultsFriends.tsx
+++ src/ui/rating/friends/ResultsFriends.tsx
@@ -1,6 +1,7 @@
 import { getFriendsRatingFX } from '@core/api/rating/effects.rating';
 import { $rating } from '@core/api/rating/store.rating';
 import { numberWithSpace, wrapAsset } from '@core/utils';
+import { contentCenter } from '@ui/theme/theme.css';
 import { typography } from '@ui/theme/typography.css';
 import { Avatar, Cell } from '@vkontakte/vkui';
 import { useStore, useStoreMap } from 'effector-react';
@@ -27,17 +28,20 @@ export const ResultsFriends = memo(() => {
         <Cell
           key={friendsr.id}
           before={
-            <div>
-              {index + 1}
+            <div className={contentCenter({ direction: 'row', gap: '1' })}>
+              <p className={typography({ variant: 'small' })}>{index + 1}</p>
               <Avatar size={40} src={friendsr.avatar} />
             </div>
           }
           size={32}
+          hasActive={false}
         >
-          <div>
-            {friendsr.firstName ?? friendsr.lastName}
-            <img src={wrapAsset('/imgs/trophy.png')} alt="trophy" width="20" height="20" />
-            <p className={typography({ variant: 'elo', m: 'l.5' })}>{numberWithSpace(friendsr.pts)}</p>
+          <div className={contentCenter({ direction: 'row', gap: '1', justifyContent: 'sb' })}>
+            <p className={typography({ variant: 'small' })}>{friendsr.firstName ?? friendsr.lastName}</p>
+            <div className={contentCenter({ direction: 'row', gap: '1' })}>
+              <img src={wrapAsset('/imgs/trophy.png')} alt="trophy" width="20" height="20" />
+              <p className={typography({ variant: 'elo', m: 'l.5' })}>{numberWithSpace(friendsr.pts)}</p>
+            </div>
           </div>
         </Cell>
       ))}
--- src/ui/rating/top100/ResultsRank100.tsx
+++ src/ui/rating/top100/ResultsRank100.tsx
@@ -1,6 +1,7 @@
 import { getTop100RankFX } from '@core/api/rating/effects.rating';
 import { $rating } from '@core/api/rating/store.rating';
 import { numberWithSpace, wrapAsset } from '@core/utils';
+import { contentCenter } from '@ui/theme/theme.css';
 import { typography } from '@ui/theme/typography.css';
 import { Avatar, Cell } from '@vkontakte/vkui';
 import { useStore, useStoreMap } from 'effector-react';
@@ -31,17 +32,20 @@ export const ResultsRank100 = memo(() => {
         <Cell
           key={top100r.id}
           before={
-            <div>
-              {index + 1}
+            <div className={contentCenter({ direction: 'row', gap: '1' })}>
+              <p className={typography({ variant: 'small' })}>{index + 1}</p>
               <Avatar size={40} src={top100r.avatar} />
             </div>
           }
           size={32}
+          hasActive={false}
         >
-          <div>
-            {top100r.firstName ?? top100r.lastName}
-            <img src={wrapAsset('/imgs/trophy.png')} alt="trophy" width="20" height="20" />
-            <p className={typography({ variant: 'elo', m: 'l.5' })}>{numberWithSpace(top100r.pts)}</p>
+          <div className={contentCenter({ direction: 'row', gap: '1', justifyContent: 'sb' })}>
+            <p className={typography({ variant: 'small' })}>{top100r.firstName ?? top100r.lastName}</p>
+            <div className={contentCenter({ direction: 'row', gap: '1' })}>
+              <img src={wrapAsset('/imgs/trophy.png')} alt="trophy" width="20" height="20" />
+              <p className={typography({ variant: 'elo', m: 'l.5' })}>{numberWithSpace(top100r.pts)}</p>
+            </div>
           </div>
         </Cell>
       ))}
