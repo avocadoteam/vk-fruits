aa1bc170ae59c18ab93ff07c01087f7eb490a8d2
Konstantin <konstantin.miheev@sensearena.com>
2023-10-25 08:16:26 +0200

    temp disable and debug useWatchLeftRoom

--- src/ui/layout/useWatchLeftRoom.ts
+++ src/ui/layout/useWatchLeftRoom.ts
@@ -1,7 +1,5 @@
 import { usePrevious } from '@core/hooks/usePrevious';
-import { leaveRoom } from '@core/sockets/game';
 import { useActiveVkuiLocation, useParams } from '@vkontakte/vk-mini-apps-router';
-import { useEffect } from 'react';
 import { FPanel } from './router';
 
 const lobbyPanels = [FPanel.Lobby, FPanel.LobbyInvited, FPanel.GameFound];
@@ -14,13 +12,18 @@ export const useWatchLeftRoom = () => {
   const lastLobbyId = usePrevious(lobbyId);
   const wasItInLobby = lastLobbyId && lastActivePanel && lobbyPanels.includes(lastActivePanel);
 
-  useEffect(() => {
-    const leaveIt = () => {
-      if (wasItInLobby) {
-        leaveRoom(lastLobbyId);
-      }
-    };
-    window.addEventListener('popstate', leaveIt);
-    return () => window.removeEventListener('popstate', leaveIt);
-  }, [wasItInLobby]);
+  console.debug('KAVO');
+  console.debug('wasItInLobby', wasItInLobby);
+  console.debug('lastLobbyId', lastLobbyId);
+  console.debug('lastActivePanel', lastActivePanel);
+
+  // useEffect(() => {
+  //   const leaveIt = () => {
+  //     if (wasItInLobby) {
+  //       leaveRoom(lastLobbyId);
+  //     }
+  //   };
+  //   window.addEventListener('popstate', leaveIt);
+  //   return () => window.removeEventListener('popstate', leaveIt);
+  // }, [wasItInLobby]);
 };
