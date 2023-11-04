6bc739a16a0935a55d3b21f5916cc8965fe2bb0e
Kon <whynunu@why.com>
2023-11-04 15:02:47 +0100

    fixed getAdsData

--- src/core/vk-bridge/ads.ts
+++ src/core/vk-bridge/ads.ts
@@ -1,7 +1,7 @@
 import { BannerAdLocation, EAdsFormats } from '@vkontakte/vk-bridge';
 import { vkBridge } from './instance';
 
-export const getAdsData = () => vkBridge.send('VKWebAppCheckNativeAds');
+export const getAdsData = () => vkBridge.send('VKWebAppCheckNativeAds', { ad_format: EAdsFormats.REWARD });
 
 export const checkAdsBanner = () => vkBridge.send('VKWebAppCheckBannerAd');
 export const showAdsBanner = () => vkBridge.send('VKWebAppShowBannerAd', { banner_location: BannerAdLocation.BOTTOM });
